f45ca3d3b480b8caa760a9efbbb2f16b5a8c06f7
Zakir-Ansari <46892550+Zakir-Ansari@users.noreply.github.com>
2019-05-15 15:21:35 +0530

    Updating password

--- SQLConnectionExample/src/sql_operation_examples/DropTable.java
+++ SQLConnectionExample/src/sql_operation_examples/DropTable.java
@@ -10,7 +10,7 @@ public class DropTable {
 
 	// Database credentials
 	static final String USER = "root";
-	static final String PASS = "Zakir@1627";
+	static final String PASS = "pass";
 
 	public static void main(String[] args) {
 		Connection conn = null;
